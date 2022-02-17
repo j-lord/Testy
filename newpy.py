758cb696835d099c00bed0df7f4cccd21bba2f63
J.Lord <followjaysin@gmail.com>
2022-02-16 21:19:36 -0600

    Notes about portfolio

--- newpy.py
+++ newpy.py
@@ -4,8 +4,13 @@ print("Can you see this here?")
 print("What about line 3?")
 print("after this, it is time to focus on real commits")
 
-print(math.radians(30))
+// the question is what type of repository it should focus on
+// a portfolio site that has several things that come to mind??
+// where you can add models that you make in blender to it
+// the real question is if the node module package can fit in the repository
+// lets look into this
 
+print(math.radians(30))
 print(math.radians(66))
 print(math.radians(67))
 print(math.radians(68))
